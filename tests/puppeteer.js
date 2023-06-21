9b0a9f8c2616f3b9e3ee27330113b6ce0f3bab15
Aleksey Kabishau <aleksey.kabishau@icloud.com>
2023-06-21 10:46:21 -0400

    Update README.md

--- tests/puppeteer.js
+++ tests/puppeteer.js
@@ -11,29 +11,46 @@ chai.should();
 
 (async () => {
   describe("Functional Tests with Puppeteer", function () {
-    let browser = null;
-    let page = null;
+    let browser, page;
+
+    // set up browser and navigate to page
     before(async function () {
       this.timeout(5000);
-      browser = await puppeteer.launch();
+      browser = await puppeteer.launch({
+        executablePath: process.env.PUPPETEER_EXECUTABLE_PATH,
+        headless: false, // Set to true if you want to run in headless mode
+      });
       page = await browser.newPage();
       await page.goto("http://localhost:3000");
     });
+
+    // clean up after tests
     after(async function () {
-      this.timeout(5000);
+      this.timeout(500000);
       await browser.close();
       server.close();
       return;
     });
+
     describe("got to site", function () {
       it("should have completed a connection", function (done) {
         done();
       });
+
+      it("should have correct page title", async function () {
+        const pageTitle = await page.title();
+        pageTitle.should.equal("Testing with Chai");
+      });
     });
+
+
     describe("people form", function () {
       this.timeout(5000);
+
+      // select element and assert that it exists
+      // this refers to the Mocha context object
       it("should have various elements", async function () {
-        this.nameField = await page.$("input[name=name]");
+        this.nameField = await page.$("input[name=name]"); 
         this.nameField.should.not.equal(null);
         this.ageField = await page.$("input[name=age]");
         this.ageField.should.not.equal(null);
@@ -48,28 +65,31 @@ chai.should();
         this.listPeople = await page.$("#listPeople");
         this.listPeople.should.not.equal(null);
       });
+
       it("should create a person record given name and age", async function () {
         await this.nameField.type("Fred");
         await this.ageField.type("10");
         await this.addPerson.click();
         await sleep(200);
-        const resultData = await (
-          await this.resultHandle.getProperty("textContent")
-        ).jsonValue();
+
+        // getProperty returns a JSHandle(object representing the property of the element)
+        const resultData = await (await this.resultHandle.getProperty("textContent")).jsonValue();
         console.log("at 1, resultData is ", resultData);
         resultData.should.include("A person record was added");
         const { index } = JSON.parse(resultData);
         this.lastIndex = index;
       });
+
       it("should not create a person record without an age", async function () {
         // your code goes here.  Hint: to clear the age field, you need the line
-        // await page.$eval("#age", (el) => (el.value = "")); 
+        await page.$eval("#age", (el) => (el.value = ""));
+        await this.addPerson.click();
       });
       it("should return the entries just created", async function () {
-         // your code goes here
+        // your code goes here
       });
       it("should return the last entry.", async function () {
-         // your code goes here
+        // your code goes here
       });
     });
   });
